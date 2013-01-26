689ee37e89e860027ada29191d79e921da73f40c
David Nelson <davidmichael.nelson@gmail.com>
2013-01-26 16:08:00 -0200

    Tomcat module instalation

---
+++ modules/tomcat/manifests/init.pp
@@ -0,0 +1,22 @@
+class tomcat {
+
+  $tomcat_port = 8080
+  
+  notice("Establishing http://$hostname:$tomcat_port/")
+
+  package { 'tomcat6':
+    ensure => installed,
+  }
+
+  user { 'tomcat6':
+    ensure => 'present',
+    home   => '/home/tomcat6',
+    shell  => '/bin/sh',
+  }
+
+  service { 'tomcat6':
+    ensure => running,
+    require => Package['tomcat6'],
+  }   
+
+}
