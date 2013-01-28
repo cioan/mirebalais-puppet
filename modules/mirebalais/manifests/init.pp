2a6b95ceb6a4476076b3b6587a8fccc5bf1e1cf0
David Nelson <davidmichael.nelson@gmail.com>
2013-01-27 23:25:27 -0200

    Fix weird indentation

--- modules/mirebalais/manifests/init.pp
+++ modules/mirebalais/manifests/init.pp
@@ -8,15 +8,15 @@ class mirebalais(
   include mirebalais::components::java
   include mirebalais::components::tomcat
 
-  class{ 'mirebalais::components::mysql' :
+  class { 'mirebalais::components::mysql':
     root_password => $mysql_root_password,
     default_db => $mysql_default_db,
     default_db_user => $mysql_default_db_user,
-    default_db_password => $mysql_default_db_password, 
+    default_db_password => $mysql_default_db_password 
   }
 
-  file { '/etc/environment' :
-		source => "puppet:///modules/mirebalais/etc/environment" ,
+  file { '/etc/environment':
+    source => "puppet:///modules/mirebalais/etc/environment"
   }  
 
   file { '/home/tomcat6/.OpenMRS':
@@ -27,8 +27,8 @@ class mirebalais(
     require => User['tomcat6']
   }
 
-  file { '/home/tomcat6/.OpenMRS/mirebalais.properties' :
-		content => template("mirebalais/OpenMRS/mirebalais.properties.erb"),
+  file { '/home/tomcat6/.OpenMRS/mirebalais.properties':
+    content => template("mirebalais/OpenMRS/mirebalais.properties.erb"),
     ensure  => present,
     owner   => tomcat6,
     group   => tomcat6,
@@ -36,8 +36,8 @@ class mirebalais(
     require => User['tomcat6']
   } 
   
-  file { '/home/tomcat6/.OpenMRS/mirebalais-runtime.properties' :
-		content => template("mirebalais/OpenMRS/mirebalais-runtime.properties.erb"),
+  file { '/home/tomcat6/.OpenMRS/mirebalais-runtime.properties':
+    content => template("mirebalais/OpenMRS/mirebalais-runtime.properties.erb"),
     ensure  => present,
     owner   => tomcat6,
     group   => tomcat6,
