475bd5731dff08e68d83175d1280eff5f93ba3c5
David Nelson <davidmichael.nelson@gmail.com>
2013-01-28 23:59:14 -0200

    Add ordering on MySQL installation

--- modules/mirebalais/manifests/components/mysql.pp
+++ modules/mirebalais/manifests/components/mysql.pp
@@ -7,43 +7,43 @@ class mirebalais::components::mysql (
 
   include mysql
 
-  file { '/etc/mysql/my.cnf':
-    source  => 'puppet:///modules/mirebalais/mysql/my.cnf',
-    ensure  => present,
-    notify  => Service['mysqld']
-  }
-
   class { 'mysql::server': 
     manage_service => false,
     config_hash => { 
       'root_password' => $root_password,  
       'config_file' => '/tmp/my.cnf',
-    }
-  }
+      'restart' => false
+    },
+  } ->
+
+  file { '/etc/mysql/my.cnf':
+    source  => 'puppet:///modules/mirebalais/mysql/my.cnf',
+    ensure  => file,
+  } ~>
 
   service { 'mysqld':
-      ensure   => running,
-      name     => 'mysql',
-      enable   => true,
-      require  => [File['/etc/mysql/my.cnf'], Package['mysql-server']],
-      provider => upstart,
-    }
+    ensure   => running,
+    name     => 'mysql',
+    enable   => true,
+    require  => [File['/etc/mysql/my.cnf'], Package['mysql-server']],
+    provider => upstart,
+  } ->
 
   database { $default_db :
     require => Service['mysqld'],
     ensure  => present,
-  }
+  } ->
 
   database_user { "${default_db_user}@localhost":
     password_hash => mysql_password($default_db_password),
     ensure  => present,
     require => Service['mysqld'],
-  }
+  } ->
 
   database_grant { "${default_db_user}@localhost/${default_db}":
     privileges => ['all'],
     require => Service['mysqld'],
-  }
+  } ->
 
   database_grant { "root@localhost/${default_db}":
     privileges => ['all'],
