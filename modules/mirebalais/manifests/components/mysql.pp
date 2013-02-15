9000cc7a8f79cd62ee2aaed650313ca0c0db859f
Fernando Freire <thenano@gmail.com>
2013-02-15 10:23:45 -0500

    adding start slave command to production slave

--- modules/mirebalais/manifests/components/mysql.pp
+++ modules/mirebalais/manifests/components/mysql.pp
@@ -87,9 +87,14 @@ class mirebalais::components::mysql (
   }
 
   if $environment == 'production_slave' {
-    exec { "master replication":
+    exec { "master replication setup":
       command => "/usr/bin/mysql -uroot -p${root_password} -e \"CHANGE MASTER TO MASTER_HOST='${master_ip}', MASTER_USER='${replication_user}', MASTER_PASSWORD='${replication_password}', MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS=245;\"",
       require => Service["mysqld"],
+    }
+
+    exec { "start slave":
+      command => "mysql -uroot -p${root_password} -e \"start slave;\"",
+      require => [ Service["mysqld"], Exec["master replication setup"] ]
     }
   }
 
