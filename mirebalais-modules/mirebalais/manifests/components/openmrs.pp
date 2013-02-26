0c41ac6c99af1e6c90590012faffa01122e41f88
Fernando Freire <thenano@gmail.com>
2013-02-26 19:52:35 -0300

    removing uneeded dependency

--- mirebalais-modules/mirebalais/manifests/components/openmrs.pp
+++ mirebalais-modules/mirebalais/manifests/components/openmrs.pp
@@ -38,7 +38,7 @@ class mirebalais::components::openmrs (
       command => "java -Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock -jar liquibase.jar --driver=com.mysql.jdbc.Driver --classpath=/usr/local/${tomcat}/webapps/mirebalais.war --url=jdbc:mysql://localhost:3306/${default_db} --changeLogFile=liquibase-schema-only.xml --username=${default_db_user} --password=${default_db_password} update",
       user    => 'root',
       unless  => "mysql -u${default_db_user} -p${default_db_password} ${default_db} -e 'desc patient'",
-      require => [ Package['mirebalais'], Database[$default_db], Exec['tomcat-stop'] ],
+      require => [ Package['mirebalais'], Database[$default_db] ],
     }
 
     exec { 'migrate core data':
