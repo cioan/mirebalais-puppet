ec91f02604bb976d3ff4653bc0b599053f94daba
Fernando Freire <thenano@gmail.com>
2013-02-28 11:11:28 -0300

    no need for apache on test env

--- mirebalais-modules/mirebalais/manifests/init.pp
+++ mirebalais-modules/mirebalais/manifests/init.pp
@@ -3,8 +3,11 @@ class mirebalais {
   include mirebalais::components::java
   include mirebalais::components::tomcat
   include mirebalais::components::openmrs
-  include mirebalais::components::apache_ssl
   include mirebalais::components::mirth
+
+  if $environment != 'test' {
+    include mirebalais::components::apache_ssl
+  }
 
   if $environment == 'production' {
     include mirebalais::components::mysql_backup
