4dcf839c538219212e365792503a1e059eb9a40b
Fernando Freire <thenano@gmail.com>
2013-03-01 14:49:24 -0300

    encrypting password and installing the keys from the dev server

--- install.sh
+++ install.sh
@@ -1,11 +1,34 @@
 #! /bin/bash
 
 if [ -z "$1" ]
+then
+  echo "You need to provide the environment to install in:"
+  echo "./install.sh ENVIRONMENT"
+  echo "ENVIRONMENT can be test|production|production_slave"
+  exit 1
+fi
+
+if [ "$1" != "test" ]
+then
+  if [ ! -f /etc/encryptor_secret_key ]
   then
-    echo "You need to provide the environment to install in:"
-    echo "./install.sh ENVIRONMENT"
-    echo "ENVIRONMENT can be test|production|production_slave"
-    exit 1
+    echo "Please provide a username to fetch private data"
+    read user
+
+    scp $user@dev.pih-emr.org:/etc/mirebalais/* .
+
+    mv encryptor_secret_key /etc/
+    mv pih-emr.org.key /etc/ssl/private/
+  fi
+
+  if [ ! -f ~/.ssh/id_dsa ]
+  then
+    ssh-keygen -q -t dsa -f ~/.ssh/id_dsa -P ''
+  fi
+
+  echo "Please make sure you have copied this ssh public key to the backup server so that database backups can be uploaded there:"
+  cat ~/.ssh/id_dsa.pub
+  read
 fi
 
 apt-get install -y rubygems
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,4 @@
 node default {
   Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
-  class { 'mirebalais' : }
+  include mirebalais
 }
