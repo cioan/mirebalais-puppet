8b2dbcf897871253c676b04fa51765a05a5f1716
Fernando Freire <thenano@gmail.com>
2013-03-04 18:36:12 -0300

    including the logging module in the default node

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,9 @@
 node default {
   Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
   include mirebalais
+
+  if $environment == 'production' {
+    include mirebalais_logging
+  }
+
 }
