3653368c7b3723b430d4af56dfb1f7c862cff529
Fernando Freire <thenano@gmail.com>
2013-03-12 19:18:54 -0300

    fixing logstash email and kibana bundle setup

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,5 @@
 node default {
-  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
+  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', 'usr/local/bin' ] }
   include mirebalais
 
   if $environment != 'test' {
