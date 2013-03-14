fe33f0867099182a47ca855b00c03bded63a274f
Fernando Freire <thenano@gmail.com>
2013-03-14 17:15:51 -0300

    using test instead of [[

--- mirebalais-modules/mirebalais/manifests/components/mysql.pp
+++ mirebalais-modules/mirebalais/manifests/components/mysql.pp
@@ -74,7 +74,7 @@ class mirebalais::components::mysql (
 
     exec { 'master replication setup':
       command => "/usr/bin/mysql -uroot -p${root_password} -e \"CHANGE MASTER TO MASTER_HOST='${master_ip}', MASTER_USER='${replication_user}', MASTER_PASSWORD='${replication_password}', MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS=245;\"",
-      unless  => "[[ ! -z `mysql -u root -p${root_password} -e 'show slave status'` ]]",
+      unless  => "test ! -z `mysql -u root -p${root_password} -e 'show slave status'`",
       require => [ Service['mysqld'], Exec['set_mysql_rootpw'] ]
     }
 
