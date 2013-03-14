325ef4fe6c06648d64d4bc92f57406e1da443cc6
Fernando Freire <thenano@gmail.com>
2013-03-14 17:14:55 -0300

    correcting the path

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,5 @@
 node default {
-  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', 'usr/local/bin' ] }
+  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/' ] }
   include mirebalais
 
   if $environment != 'test' {
