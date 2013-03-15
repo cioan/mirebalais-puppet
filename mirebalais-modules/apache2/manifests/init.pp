78eb4b5e6980c12b34d9d136afc9a84cbb850d3d
Fernando Freire <thenano@gmail.com>
2013-03-15 20:25:33 -0300

    adding the refresh of ntpdate, fixing apache mods subscribe

--- mirebalais-modules/apache2/manifests/init.pp
+++ mirebalais-modules/apache2/manifests/init.pp
@@ -50,7 +50,7 @@ class apache2 (
   exec { 'enable apache mods':
     command     => 'a2enmod jk && a2enmod deflate && a2enmod ssl && a2ensite default-ssl && a2enmod rewrite',
     user        => 'root',
-    subscribe   => [ Service[$tomcat], Package['apache2'], Package['libapache2-mod-jk'] ],
+    subscribe   => [ Package['apache2'], Package['libapache2-mod-jk'] ],
     refreshonly => true,
     notify      => Service['apache2']
   }
--- mirebalais-modules/ntpdate/manifests/init.pp
+++ mirebalais-modules/ntpdate/manifests/init.pp
@@ -2,4 +2,10 @@ class ntpdate {
   file { '/etc/ntp.conf':
     source => 'puppet:///modules/ntpdate/etc/ntp.conf'
   }
+
+  exec { 'update time':
+    command     => 'ntpdate-debian',
+    subscribe   => File['/etc/ntp.conf'],
+    refreshonly => true
+  }
 }
