d267f65abdcefe131e3389d9ca93725c52d0ef74
Fernando Freire <thenano@gmail.com>
2013-03-15 21:30:18 -0300

    trying to fool the exec unless

--- mirebalais-modules/openmrs/manifests/liquibase_migrate.pp
+++ mirebalais-modules/openmrs/manifests/liquibase_migrate.pp
@@ -1,6 +1,6 @@
 define openmrs::liquibase_migrate(
   $dataset,
-  $unless = false,
+  $unless = '`exit 1`',
   $openmrs_db = hiera('openmrs_db'),
   $openmrs_db_user = decrypt(hiera('openmrs_db_user')),
   $openmrs_db_password = decrypt(hiera('openmrs_db_password')),
